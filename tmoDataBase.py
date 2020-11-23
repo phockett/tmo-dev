2cea9036a29e9c7d73906c8668dab9a90ad5d7c3
phockett <phockett@users.noreply.github.com>
2020-11-23 14:58:39 -0500

    Debug.

--- tmoDataBase.py
+++ tmoDataBase.py
@@ -196,24 +196,24 @@ class tmoDataBase():
 #**** PLOTTING STUFF
 
     def isnotebook():
-    """
-    Check if code is running in Jupyter Notebook.
+        """
+        Check if code is running in Jupyter Notebook.
 
-    Taken verbatim from https://exceptionshub.com/how-can-i-check-if-code-is-executed-in-the-ipython-notebook.html
-    Might be a better/more robust way to do this?
+        Taken verbatim from https://exceptionshub.com/how-can-i-check-if-code-is-executed-in-the-ipython-notebook.html
+        Might be a better/more robust way to do this?
 
-    """
+        """
 
-    try:
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True   # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False      # Probably standard Python interpreter
+        try:
+            shell = get_ipython().__class__.__name__
+            if shell == 'ZMQInteractiveShell':
+                return True   # Jupyter notebook or qtconsole
+            elif shell == 'TerminalInteractiveShell':
+                return False  # Terminal running IPython
+            else:
+                return False  # Other type (?)
+        except NameError:
+            return False      # Probably standard Python interpreter
 
 
     def showPlot(self, dim, run, pType='curve'):
